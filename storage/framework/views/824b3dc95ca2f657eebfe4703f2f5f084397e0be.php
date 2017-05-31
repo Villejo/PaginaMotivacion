9b9d71aa59b9bc87a650b4dd6a7c257e91cba255
smallvillejo <jorge_9128@hotmail.com>
2017-05-31 08:29:04 -0500

    mensaje

--- storage/framework/views/824b3dc95ca2f657eebfe4703f2f5f084397e0be.php
+++
@@ -1,6 +0,0 @@
-<?php $__env->startSection('title'); ?>
-Gana dinero desde Casa
-<?php $__env->stopSection(); ?>
-<?php $__env->startSection('content'); ?>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('layouts.master', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
